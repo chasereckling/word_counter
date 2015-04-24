a4d0aa0d855382ceab56deb3485e46d93e0686e6
chase <chase.reckling@gmail.com>
2015-04-24 09:16:12 -0700

    write first spec to split words to a string

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -6,5 +6,4 @@ require('word_counter')
     it('split the given string into an array') do
      expect(("my cat is a black cat").word_counter()).to(eq(["my", "cat", "is", "a", "black", "cat"]))
    end
-   it('find the given input word ')
  end
