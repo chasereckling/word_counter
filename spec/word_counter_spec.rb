49beda99951708a09a3f0cf50fab963a47c00211
chase <chase.reckling@gmail.com>
2015-04-24 14:05:59 -0700

    write spec when given input word does not occur in the given string return 0, spec pass

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -16,4 +16,7 @@ require('pry')
     it('count number of instances the given input word occurs in the new array') do
      expect(("my cat is a black cat").word_counter("cat")).to(eq(2))
     end
- end
+    it('display the number 0 if the sentence includes no instances of the given input') do
+     expect(("my cat is a black cat").word_counter("dog")).to(eq(0))
+   end
+  end
