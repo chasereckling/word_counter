3134d52721f4ad2faebbff3dd496840d15206ef7
chase <chase.reckling@gmail.com>
2015-04-24 13:41:48 -0700

    write spec to count number of instances the given input word occurs in the given string, display fixnumas fixnum

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -10,9 +10,10 @@ require('pry')
     it('check if new given string includes the given input word') do
      expect(("my cat is a black cat").word_counter("cat")).to include("cat")
      end
-    it('display number of instances the given word occurs in the given string') do
+    it('find number of instances the given word occurs in the given string, push to new array') do
      expect(("my cat is a black cat").word_counter("cat")).to(eq(["cat", "cat"]))
     end
-    it('') do
+    it('count number of instances the given input word occurs in the new array') do
+     expect(("my cat is a black cat").word_counter("cat")).to(eq('2'))
     end
  end
