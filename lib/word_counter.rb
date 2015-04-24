79e91a2518684328958a6e8ebabad6fe6689620f
chase <chase.reckling@gmail.com>
2015-04-24 12:09:27 -0700

    write spec to count number of times the given input word occurrs in the new array

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -3,13 +3,14 @@ class String
   define_method(:word_counter) do
     input_string = self
     split_string = input_string.split()
-    word_count = []
+    word_count = 0
     count_string = []
 
      split_string.each() do |word|
       if split_string.include?(word)
-
+       word_count = word_count.+(1)
     end
   end
+  word_count
 end
 end
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -4,10 +4,13 @@ require('pry')
 
   describe('String#word_counter') do
 
-  #   it('split the given input string into an array') do
-  #    expect(("my cat is a black cat").word_counter()).to(eq(["my", "cat", "is", "a", "black", "cat"]))
-  #  end
-    it('check if new split string includes the given input word') do
+     it('split the given input string into a new array') do
+      expect(("my cat is a black cat").word_counter()).to(eq(["my", "cat", "is", "a", "black", "cat"]))
+    end
+    it('check if new array includes the given input word') do
      expect(("my cat is a black cat").word_counter()).to include("cat")
      end
+    it ('count how many times the given word occurs in the new array') do
+     expect(("my cat is a black cat").word_counter()).to(eq(2))
+    end
  end
