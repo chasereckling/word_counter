bcdb750975820d8e33b58f5a5ae05bc0bdfade19
chase <chase.reckling@gmail.com>
2015-04-24 11:03:34 -0700

    add Gemfile, bundle install

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,6 +1,14 @@
 class String
 
   define_method(:word_counter) do
-    input_string = self.split()
+    input_string = self
+    split_string = input_string.split()
+    word_count = 0
+
+    split_string.each() do |input_word|
+    if split_string.include?(input_word)
+      #word_count = word_count.+(1)
+    end
   end
-end
+  end
+  end
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -1,12 +1,13 @@
 require('rspec')
 require('word_counter')
+require('pry')
 
   describe('String#word_counter') do
 
     it('split the given input string into an array') do
      expect(("my cat is a black cat").word_counter()).to(eq(["my", "cat", "is", "a", "black", "cat"]))
    end
-    it('check if that new array includes the given input word') do
-     expect((["my", "cat", "is", "a", "black", "cat"]).word_counter(input_word)).to(eq(true))
+    it('check if new split string includes the given input word') do
+     expect(("my cat is a black cat").word_counter("cat")).to(eq(true))
      end
  end
