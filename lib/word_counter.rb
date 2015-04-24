a1d15608af32fec79da1e786d0c8f57adc82c7f7
chase <chase.reckling@gmail.com>
2015-04-24 09:23:16 -0700

    write first test for split string into array, spec pass

---
+++ lib/word_counter.rb
@@ -0,0 +1,6 @@
+class String
+
+  define_method(:word_counter) do
+    input_string = self.split()
+  end
+end
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -1,7 +1,7 @@
 require('rspec')
 require('word_counter')
 
-  describe('String#word_counter')
+  describe('String#word_counter') do
 
     it('split the given string into an array') do
      expect(("my cat is a black cat").word_counter()).to(eq(["my", "cat", "is", "a", "black", "cat"]))
