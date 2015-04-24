fb9fbca0f74f6c3d8702d762fb720d73a9b7976e
chase <chase.reckling@gmail.com>
2015-04-24 09:33:47 -0700

    write spec to check if new array includes a given input word

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -3,7 +3,10 @@ require('word_counter')
 
   describe('String#word_counter') do
 
-    it('split the given string into an array') do
+    it('split the given input string into an array') do
      expect(("my cat is a black cat").word_counter()).to(eq(["my", "cat", "is", "a", "black", "cat"]))
    end
+    it('check if that new array includes the given input word') do
+     expect((["my", "cat", "is", "a", "black", "cat"]).word_counter(input_word)).to(eq(true))
+     end
  end
