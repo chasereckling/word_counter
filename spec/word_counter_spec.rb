cd5f090aec521a4f8c1722721f507c3841e0502b
Chase Reckling <chasereckling@gmail.com>
2015-06-02 20:57:30 -0700

    Update word_counter_spec.rb

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -4,15 +4,6 @@ require('pry')
 
   describe('String#word_counter') do
 
-    # it('split the given input string into a new array') do
-    #  expect(("my cat is a black cat").word_counter("cat")).to(eq(["my", "cat", "is", "a", "black", "cat"]))
-    # end
-    # it('check if new given string includes the given input word') do
-    #  expect(("my cat is a black cat").word_counter("cat")).to include("cat")
-    #  end
-    # it('find number of instances the given word occurs in the given string, push to new array') do
-    #  expect(("my cat is a black cat").word_counter("cat")).to(eq(["cat", "cat"]))
-    # end
     it('count number of instances the given input word occurs in the given string') do
      expect(("my cat is a black cat").word_counter("cat")).to(eq(2))
     end
